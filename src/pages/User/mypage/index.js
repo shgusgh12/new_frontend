a93a16ed4686e43055807353a43104b11288bb9f
toothlessdev <toothless042@gmail.com>
2023-09-22 14:02:42 +0900

    feat: 네비게이션 바 수정

--- src/pages/User/mypage/index.js
+++ src/pages/User/mypage/index.js
@@ -1,18 +1,20 @@
 import LeftBar from "../../../components/LeftBar";
+import { NavAside } from "../../../components/Nav";
 import MyPageBody from "./body";
 import styled from "styled-components";
 
 function MyPage() {
-  return (
-    <MyPageContainer>
-      <LeftBar />
-      <MyPageBody />
-    </MyPageContainer>
-  );
+    return (
+        <MyPageContainer>
+            <NavAside />
+            <MyPageBody />
+        </MyPageContainer>
+    );
 }
 
 const MyPageContainer = styled.div`
-  display: flex;
+    display: flex;
+    margin-top: 40px;
 `;
 
 export default MyPage;
