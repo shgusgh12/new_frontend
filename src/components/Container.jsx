874653eaca06ddee22960726307118879a5681e4
toothlessdev <toothless042@gmail.com>
2023-09-22 14:19:51 +0900

    rename: Module Scss 로 확장자 변경

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,8 @@
 import classNames from "classnames";
-import styles from "./Container.module.css";
+import styles from "./Container.module.scss";
 
 function Container({ className, children }) {
-  return (
-    <div className={classNames(className, styles.container)}>{children}</div>
-  );
+    return <div className={classNames(className, styles.container)}>{children}</div>;
 }
 
 export default Container;
