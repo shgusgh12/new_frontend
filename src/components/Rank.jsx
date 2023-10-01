2a36e23d403157a5e1739c571d746e230d10a452
shgusgh12 <https://github.com/shgusgh12>
2023-10-01 19:00:52 +0900

    feat: 관심종목 메뉴 구현

---
+++ src/components/Rank.jsx
@@ -0,0 +1,62 @@
+import { Table } from "./Table";
+import styles from './Rank.module.scss';
+
+
+const Rank = ({name}) => {
+  
+    if(name === '관심종목'){
+      //관심종목일때 받아온 데이터
+    }
+    else{
+      //아닐때
+    }
+    return(
+      <div className={styles.rank}>
+        <Table.Container  ratio={[1,3,1,1]} width="100%" padding='40px'>
+          <h2>{name}</h2>
+          <Table.Head>
+            <Table.Row borderBottom fontSize='14px' color='#989898' height="50px">
+                <Table.Col position="center-left">순위</Table.Col>
+                <Table.Col position="center-center">종목명</Table.Col>
+                <Table.Col position="center-center">현재가</Table.Col>
+                <Table.Col position="center-right">등락률</Table.Col>
+            </Table.Row>
+          </Table.Head>
+          <Table.Body>
+            <Table.Row height="50px" fontSize='14px'>
+              <Table.Col position='center-left'>#1</Table.Col>
+              <Table.Col>삼성전자</Table.Col>
+              <Table.Col>55,100</Table.Col>
+              <Table.Col position="center-right">11.8%</Table.Col>
+            </Table.Row>
+            <Table.Row height="55px" fontSize='14px'>
+              <Table.Col position='center-left'>#2</Table.Col>
+              <Table.Col>AAPL</Table.Col>
+              <Table.Col>55,100</Table.Col>
+              <Table.Col position="center-right">7.31%</Table.Col>
+            </Table.Row>
+            <Table.Row height="55px" fontSize='14px'>
+              <Table.Col position='center-left'>#3</Table.Col>
+              <Table.Col>INVSC QQQ TRUST SRS</Table.Col>
+              <Table.Col>55,100</Table.Col>
+              <Table.Col position="center-right">11.8%</Table.Col>
+            </Table.Row>
+            <Table.Row height="55px" fontSize='14px'>
+              <Table.Col position='center-left'>#4</Table.Col>
+              <Table.Col>AAPL</Table.Col>
+              <Table.Col>55,100</Table.Col>
+              <Table.Col position="center-right">7.31%</Table.Col>
+            </Table.Row>
+            <Table.Row height="55px" fontSize='14px'>
+              <Table.Col position='center-left'>#5</Table.Col>
+              <Table.Col>AAPL</Table.Col>
+              <Table.Col>55,100</Table.Col>
+              <Table.Col position="center-right">7.31%</Table.Col>
+            </Table.Row>  
+          </Table.Body>
+        </Table.Container>
+      </div>
+    );
+  }
+  
+export default Rank;
--- src/pages/User/experiment/index.js
+++ src/pages/User/experiment/index.js
@@ -1,7 +1,35 @@
+import Container from '../../../components/Container';
+import Favorite from './body/Favorite';
+import Order from './body/Order';
+import Account from './body/Account';
+import styles from './Experiment.module.scss';
+import React, { useState } from 'react';
 function Experiment() {
+  const [select, setSelect] = useState(0);
+  const buttons = [
+    { text: '관심종목' },
+    { text: '포트폴리오' },
+    { text: '주식주문' },
+  ];
   return (
-    <div>
-      <h1>실험 페이지</h1>
+    <div className={styles.container}>
+      <div style={{ marginBottom: '23px' }}>
+        <div className={styles.menu}>
+          {buttons.map((item, index) => {
+            return (
+              <>
+                <button
+                  className={`${select === index ? styles['selected'] : ''}`}
+                  onClick={() => setSelect(index)}
+                >
+                  {item.text}
+                </button>
+              </>
+            );
+          })}
+        </div>
+      </div>
+      {[<Favorite></Favorite>, <Account></Account>, <Order></Order>][select]}
     </div>
   );
 }
